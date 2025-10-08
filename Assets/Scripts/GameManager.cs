c007116f2a696ab2ae88c2989761b62bfea836c5
Nur <nur.yuccel@gmail.com>
2025-10-08 14:59:45 +0200

    updated GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,19 +7,11 @@ public class GameManager : MonoBehaviour
 {
     public bool isGameOver;
     public TMP_Text gameOverText;
-    public TextMeshProUGUI waveText;
-
-    private SpawnManager spawnManager;
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         isGameOver = false;
         gameOverText.gameObject.SetActive(false);
-
-        spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
-        UpdateWaveText();
 
     }
 
@@ -39,23 +31,19 @@ public class GameManager : MonoBehaviour
         gameOverText.gameObject.SetActive(true);
         gameOverText.text = "GAME OVER\n\n Press 'R' to Restart";
 
+       
         GameObject[] powerups = GameObject.FindGameObjectsWithTag("Powerup");
         foreach (GameObject p in powerups)
         {
             Destroy(p);
         }
 
-        
+  
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (GameObject e in enemies)
         {
             Destroy(e);
         }
-    }
-
-    public void UpdateWaveText()
-    {
-        waveText.text = "Wave: " + spawnManager.waveNumber;
     }
 
 }
